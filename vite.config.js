e5d78507002ebf4bf2aef7a13bdc522bb265d8b9
鸭脖yaboo <863176846@qq.com>
2021-12-21 08:07:18 +0800

    更新配置

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,12 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/uml-exercise/',
+  base: './uml-exercise/',
+  resolve: {
+    alias: {
+      '/assets': 'src/assets',
+    },
+  },
   server: {
     proxy: {
       '/api': {
